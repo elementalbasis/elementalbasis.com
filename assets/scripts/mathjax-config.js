9041309a0b72fb4f1fa2931d01031ba4c7eb0df9
Javier Castro <javier@elementalbasis.com>
2026-04-23 22:22:41 -0400

    Moved the MathJax config from danetiska.com to elementalbasis.com

--- assets/scripts/mathjax-config.js
+++ assets/scripts/mathjax-config.js
@@ -1,21 +1,15 @@
 window.MathJax = {
-  loader: {load: ['[tex]/physics', '[tex]/cancel']},
+  loader: {load: ['[tex]/physics', '[tex]/ams', '[tex]/cancel']},
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    packages: {'[+]': ['physics']},
+    inlineMath: {'[+]': [['$', '$']]},
+    displayMath: {'[+]': [['$$', '$$']]},
     processEscapes: true,
-    packages: ['base', 'ams', 'physics', 'cancel', 'require', 'siunitx']
-    /*macros: {
-      d: "\\mathrm{d}",
-      dd: ["\\frac{\\mathrm{d}#1}{\\mathrm{d}#2}", 2],
-      pd: "\\partial",
-      pp: ["\\frac{\\partial #1}{\\partial #2}", 2]
-    }
-    */
   },
+  /*
   options: {
     skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
     ignoreHtmlClass: 'no-mathjax',
   }
+  */
 };
-
